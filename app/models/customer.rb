975a6630b683e468876dfd0cf8c252f0ec5f7c9d
Djisu <pfleischer2002@yahoo.co.uk>
2019-07-24 17:32:38 -0700

    another commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
-    validates :balance, presence: true
+    has_one :Address
+    validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true
     validates :birth_date,presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_212541) do
+ActiveRecord::Schema.define(version: 2019_07_24_223942) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.integer "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_addresses_on_customer_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
